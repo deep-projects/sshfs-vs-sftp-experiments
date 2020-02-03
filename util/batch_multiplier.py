becc94f483e4d09af15969b2ce56d698843c9f4d
Bluemi <bruno.schilling@protonmail.ch>
2020-02-03 15:38:40 +0100

    improved batch_multiplier

--- util/batch_multiplier.py
+++ util/batch_multiplier.py
@@ -1,33 +1,35 @@
 #!/usr/bin/python3
 
 
-import sys
+import argparse
 import os.path
 import copy
 import yaml
 
 
-def main():
-    if len(sys.argv) != 3:
-        print('usage: {} SOURCE_FILENAME NUM_BATCHES'.format(sys.argv[0]))
-        return
+def get_arguments():
+    parser = argparse.ArgumentParser(description='Multiply batches of a RED experiment.')
 
-    source_filename = sys.argv[1]
-    data = load_data(source_filename)
-    if data is None:
-        return
+    parser.add_argument('infile', type=str, help='The template RED file whose batch is multiplied')
+    parser.add_argument('num-batches', type=int, help='The number of batches in the result file')
+
+    return parser.parse_args()
+
+
+def main():
+    args = get_arguments()
+
+    data = load_data(args.infile)
 
     # create new batches
     first_batch = data['batches'][0]
     new_batches = []
-    num_batches = int(sys.argv[2])
-    for i in range(num_batches):
+    for i in range(vars(args)['num-batches']):
         new_batch = copy.deepcopy(first_batch)
-        new_batch['outputs']['out_file']['connector']['access']['url'] = 'http://141.45.81.204:5000/remote_out{}.txt'.format(i)
         new_batches.append(new_batch)
 
     data['batches'] = new_batches
-    target_filename = '{}_extended.yml'.format(os.path.splitext(source_filename)[0])
+    target_filename = '{}_extended.yml'.format(os.path.splitext(args.infile)[0])
     dump_data(target_filename, data)
 
 
@@ -39,13 +41,12 @@ def dump_data(target_filename, data):
             print('could not dump')
 
 
-def load_data(source_filename):
-    with open(source_filename, 'r') as f:
+def load_data(infile):
+    with open(infile, 'r') as f:
         try:
             data = yaml.safe_load(f)
         except Exception:
-            print('could not load file')
-            return None
+            raise IOError('could not load file')
     return data
 
 
