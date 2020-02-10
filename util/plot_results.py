2c27d2da0bb62117ea1791a635ad23dfe2f7c7bd
Bluemi <bruno.schilling@protonmail.ch>
2020-02-10 17:13:51 +0100

    removed old experiment ids

--- util/plot_results.py
+++ util/plot_results.py
@@ -11,33 +11,9 @@ NUM_BATCHES_LABEL = 'number of batches'
 PROCESSING_DURATION_LABEL = 'processing duration in seconds'
 SCHEDULING_DURATION_LABEL = 'scheduling duration in seconds'
 
-# noinspection SpellCheckingInspection
-ECHO_EXPERIMENT_IDS = [
-    '5e397444eeaf898b6e4b4353',  # 10
-    '5e3975513fa6fb7dd4a35747',  # 100
-    '5e397635eeaf898b6e4b4383',  # 1000
-    '5e397850eeaf898b6e4b487c',  # 10000
-]
-# noinspection SpellCheckingInspection
-CP_EXPERIMENT_IDS = [
-    '5e3d440d422ca7d7b849ce8e',  # 5G 1
-    '5e41475da167621fb065befb',  # 5G 5
-    '5e3d48d87dd16bb7aba7d9b2',  # 5G 10
-    '5e4148b5a167621fb065bf34',  # 5G 15
-    '5e414bb5a167621fb065bf8f',  # 5G 20
-]
-# noinspection SpellCheckingInspection
-SSHFS_EXPERIMENT_IDS = [
-    '5e3d81c7a167621fb065bde9',  # 5G 1
-    '5e4148331ea1eede9a9296d1',  # 5G 5
-    '5e41461ea167621fb065bee3',  # 5G 10
-    '5e414a3a1ea1eede9a92971c',  # 5G 15
-    '5e3d8242a167621fb065bdf3',  # 5G 20
-    # '5e3d82cea167621fb065be20',  # 5G 100
-]
 STATE = 'processing'
 
-EXPERIMENT_IDS = SSHFS_EXPERIMENT_IDS + CP_EXPERIMENT_IDS
+EXPERIMENT_IDS = []
 
 
 def get_arguments():
