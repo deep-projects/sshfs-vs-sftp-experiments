88f6b89e4913d6037a17a6118a14aa7e79325d19
Bluemi <bruno.schilling@protonmail.ch>
2020-02-27 13:20:16 +0100

    implemented analyse function

--- src/plot_results.py
+++ src/plot_results.py
@@ -1,6 +1,7 @@
 import pandas as pd
 
-from create_csv import PROCESSING_DURATION_CSV_PATH, plot_data_frames
+from create_csv import PROCESSING_DURATION_CSV_PATH, plot_data_frames, PROCESSING_DURATION_LABEL, \
+    NUM_CONCURRENT_BATCHES_LABEL, MOUNT_LABEL
 
 
 def get_data_frames():
@@ -9,9 +10,40 @@ def get_data_frames():
     return successful_processing_time_df
 
 
+def analyse_data_frame(duration_data_frame):
+    data_frame_5_concurrent_batches_sftp = duration_data_frame[
+        (duration_data_frame[NUM_CONCURRENT_BATCHES_LABEL] == 5) & (duration_data_frame[MOUNT_LABEL] == 'sftp')
+        ]
+    data_frame_5_concurrent_batches_sshfs = duration_data_frame[
+        (duration_data_frame[NUM_CONCURRENT_BATCHES_LABEL] == 5) & (duration_data_frame[MOUNT_LABEL] == 'sshfs')
+        ]
+
+    data_frame_30_concurrent_batches_sftp = duration_data_frame[
+        (duration_data_frame[NUM_CONCURRENT_BATCHES_LABEL] == 30) & (duration_data_frame[MOUNT_LABEL] == 'sftp')
+        ]
+    data_frame_30_concurrent_batches_sshfs = duration_data_frame[
+        (duration_data_frame[NUM_CONCURRENT_BATCHES_LABEL] == 30) & (duration_data_frame[MOUNT_LABEL] == 'sshfs')
+        ]
+
+    print('median sftp processing duration for 5 concurrent batches: {:.2f}'.format(
+        data_frame_5_concurrent_batches_sftp[PROCESSING_DURATION_LABEL].median()
+    ))
+    print('median sshfs processing duration for 5 concurrent batches: {:.2f}'.format(
+        data_frame_5_concurrent_batches_sshfs[PROCESSING_DURATION_LABEL].median()
+    ))
+    print('median sftp processing duration for 30 concurrent batches: {:.2f}'.format(
+        data_frame_30_concurrent_batches_sftp[PROCESSING_DURATION_LABEL].median()
+    ))
+    print('median sshfs processing duration for 30 concurrent batches: {:.2f}'.format(
+        data_frame_30_concurrent_batches_sshfs[PROCESSING_DURATION_LABEL].median()
+    ))
+
+
 def main():
     duration_data_frame = get_data_frames()
     plot_data_frames(duration_data_frame)
+
+    analyse_data_frame(duration_data_frame)
 
 
 if __name__ == '__main__':
